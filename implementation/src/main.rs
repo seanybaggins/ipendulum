2c6e95f98987aa3b030daf358a3eef4775ebf0ed
seanybaggins <seli2854@colorado.edu>
2021-01-06 22:23:26 +0000

    successful semihosting prints

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -2,15 +2,18 @@
 #![no_main]
 
 // defining panic behavior
-use panic_semihosting as _;
+use panic_halt as _;
 
 use stm32f3xx_hal::prelude::*;
+use cortex_m_semihosting::{hprintln, dbg};
 
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
     
+    hprintln!("hello world :)");
+
     loop {
         panic!("Testing panics :)")
     }
