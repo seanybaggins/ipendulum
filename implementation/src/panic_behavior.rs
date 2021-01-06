9cb8acf6491142ed2b72cfaaf62d56b6a716c7c8
seanybaggins <seli2854@colorado.edu>
2021-01-06 16:54:40 +0000

    about to delete .cargo/config

---
+++ implementation/src/panic_behavior.rs
@@ -0,0 +1,9 @@
+use rtt_target::{rprintln};
+use core::panic::PanicInfo;
+
+#[inline(never)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rprintln!("{}", info);
+    loop {} // You might need a compiler fence in here.
+}
