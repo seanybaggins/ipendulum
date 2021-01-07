a666ef277a307aa36e15f5d54f7ee352af8aea60
seanybaggins <seli2854@colorado.edu>
2021-01-07 17:00:47 +0000

    trying to figure out why cargo test does not work

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -1,8 +1,8 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 
 // defining panic behavior
-use panic_halt as _;
+use panic_semihosting as _;
 
 use stm32f3xx_hal::prelude::*;
 use cortex_m_semihosting::{hprintln, dbg};
@@ -13,8 +13,22 @@ use cortex_m_rt::entry;
 fn main() -> ! {
     
     hprintln!("hello world :)");
-
+    
     loop {
         panic!("Testing panics :)")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn add_two_works() {
+        assert_eq!(1, 1)
+    }
+
+    #[test] 
+    fn will_fail() {
+        assert_eq!(1, 2);
     }
 }
