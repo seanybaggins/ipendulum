7740a6b2278ab2cccd0c4acf345cf33c35bbd6fc
seanybaggins <seli2854@colorado.edu>
2021-01-14 16:59:38 -0800

    end of day commit

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -1,16 +1,17 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+use core::u16;
+
 // Allows for communication back to host during panics and dubugging
 use cortex_m_semihosting::{dbg, hprintln};
 use panic_semihosting as _;
 
-use stm32f3xx_hal as hal;
 use hal::prelude::*;
 use hal::pwm;
+use stm32f3xx_hal as hal;
 
 use cortex_m::asm;
-
 use cortex_m_rt::entry;
 
 #[cfg_attr(not(test), entry)]
@@ -33,14 +34,14 @@ fn main() -> ! {
         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
     // Configuring PWM
-    let resolution = 250;
+    let resolution = u16::MAX;
     let frequency = 1.hz();
-    let pwm_channel_no_pins =
-        pwm::tim16(device_peripherals.TIM16, resolution, frequency, &clocks);
-    let mut gpiob = device_peripherals.GPIOB.split(&mut reset_and_control_clock.ahb);
+    let pwm_channel_no_pins = pwm::tim16(device_peripherals.TIM16, resolution, frequency, &clocks);
+    let mut gpiob = device_peripherals
+        .GPIOB
+        .split(&mut reset_and_control_clock.ahb);
     let pb8 = gpiob.pb8.into_af1(&mut gpiob.moder, &mut gpiob.afrh);
-    let mut pwm_channel = pwm_channel_no_pins
-        .output_to_pb8(pb8);
+    let mut pwm_channel = pwm_channel_no_pins.output_to_pb8(pb8);
     pwm_channel.set_duty(pwm_channel.get_max_duty() / 2);
     //dbg!(pwm_channel.get_max_duty());
     //dbg!(pwm_channel.get_duty());
@@ -59,7 +60,7 @@ fn main() -> ! {
 mod tests {
     use super::*;
     #[test]
-    fn add_two_works() {
+    fn will_work() {
         assert_eq!(1, 1)
     }
 
