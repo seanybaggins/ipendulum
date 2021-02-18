5b4fae565bec13e57b8a26d9e1010f80983915c7
seanybaggins <seli2854@colorado.edu>
2021-02-18 07:13:56 -0800

    about to update logging.

--- implementation/src/init.rs
+++ implementation/src/init.rs
@@ -1,26 +1,24 @@
-use stm32f3xx_hal as hal;
-use hal::prelude::*;
+use hal::{gpio::ExtiPin, prelude::*};
 use hal::{
     gpio::{
-        gpioa::{self, PA1, PA3},
-        gpiob::{self, PB6, PB7, PB8},
-        gpioe::{self, PE13, PE15},
+        gpioa::{PA1, PA3},
+        gpiob::{PB6, PB7, PB8},
+        gpioe::{PE13, PE15},
+        Edge,
     },
-    gpio::{AF1, Input, PullUp, PushPull, Output},
-    pwm,
+    gpio::{Input, Output, PullUp, PushPull, AF1},
+    pac, pwm,
     pwm::{PwmChannel, WithPins, TIM16_CH1},
-    pac,
-    pac::TIM16,
-    rcc::Clocks,
 };
+use stm32f3xx_hal as hal;
 
-pub type PendulumEncoder = es38::Encoder<PendulumEncoderIn1, PendulumEncoderIn2>;
-type PendulumEncoderIn1 = PE13<Input<PullUp>>;
-type PendulumEncoderIn2 = PE15<Input<PullUp>>;
+pub type PendulumEncoder = es38::Encoder<PendulumEncIn1, PendulumEncIn2>;
+type PendulumEncIn1 = PE13<Input<PullUp>>;
+type PendulumEncIn2 = PE15<Input<PullUp>>;
 
-pub type CartEncoder = es38::Encoder<CartEncoderIn1, CartEncoderIn2>;
-type CartEncoderIn1 = PA1<Input<PullUp>>;
-type CartEncoderIn2 = PA3<Input<PullUp>>;
+pub type CartEncoder = es38::Encoder<CartEncIn1, CartEncIn2>;
+type CartEncIn1 = PA1<Input<PullUp>>;
+type CartEncIn2 = PA3<Input<PullUp>>;
 
 pub type MotorDriver = l298n::Motor<MotorDriverOut1, MotorDriverOut2, MotorDriverPwm>;
 type MotorDriverOut1 = PB6<Output<PushPull>>;
@@ -40,6 +38,9 @@ impl Hardware {
         let device_peripherals = pac::Peripherals::take().unwrap();
         let mut flash = device_peripherals.FLASH.constrain();
         let mut reset_and_control_clock = device_peripherals.RCC.constrain();
+        let mut syscfg = device_peripherals.SYSCFG;
+        let mut exti = device_peripherals.EXTI;
+
         let clocks = reset_and_control_clock
             .cfgr
             .sysclk(16.mhz())
@@ -56,60 +57,61 @@ impl Hardware {
             .GPIOA
             .split(&mut reset_and_control_clock.ahb);
 
-        let (motor_driver, _) = Self::setup_motor(device_peripherals.TIM16, &clocks, gpiob);
-        let pendulum_encoder = Self::setup_pendulum_encoder(gpioe);
-        let cart_encoder = Self::setup_cart_encoder(gpioa);
-        
-    }
-
-    /// Configures the motor
-    fn setup_motor(timer: TIM16, clocks: &Clocks, mut gpiob: gpiob::Parts) -> MotorDriver {
+        // Motor setup
+        let pb8: MotorDriverPwmPin = gpiob.pb8.into_af1(&mut gpiob.moder, &mut gpiob.afrh);
         let pwm_channel_no_pins = pwm::tim16(
-            timer,
+            device_peripherals.TIM16,
             u16::MAX, // resolution
             50.hz(),  // frequency
             &clocks,
         );
-
-        let pb8: MotorDriverPwmPin = gpiob.pb8.into_af1(&mut gpiob.moder, &mut gpiob.afrh);
         let pwm_channel: MotorDriverPwm = pwm_channel_no_pins.output_to_pb8(pb8);
-        let out1: MotorDriverOut1 = gpiob
+        let out1 = gpiob
             .pb6
             .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-        let out2: MotorDriverOut2 = gpiob
+        let out2 = gpiob
             .pb7
             .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+        let motor_driver: MotorDriver = l298n::Motor::new(out1, out2, pwm_channel);
 
-        let motor = l298n::Motor::new(out1, out2, pwm_channel);
-
-        (motor, gpiob)
-    }
-
-    fn setup_pendulum_encoder(mut gpioe: gpioe::Parts) -> PendulumEncoder {
-        let a_pe13 = gpioe
+        // Pendulum encoder setup
+        let mut a_pe13: PendulumEncIn1 = gpioe
             .pe13
             .into_pull_up_input(&mut gpioe.moder, &mut gpioe.pupdr);
-        let b_pe15 = gpioe
+        a_pe13.make_interrupt_source(&mut syscfg);
+        a_pe13.trigger_on_edge(&mut exti, Edge::Rising);
+        a_pe13.enable_interrupt(&mut exti);
+
+        let b_pe15: PendulumEncIn2 = gpioe
             .pe15
             .into_pull_up_input(&mut gpioe.moder, &mut gpioe.pupdr);
         let origin_offset_counts = 180;
         let counts_per_rev = 600;
 
-        es38::Encoder::new(a_pe13, b_pe15, counts_per_rev, origin_offset_counts)
-    }
+        let pendulum_encoder: PendulumEncoder =
+            es38::Encoder::new(a_pe13, b_pe15, counts_per_rev, origin_offset_counts);
 
-    fn setup_cart_encoder(mut gpioa: gpioa::Parts) -> CartEncoder {
-        let a_pa1 = gpioa
+        // Cart encoder setup
+        let mut a_pa1: CartEncIn1 = gpioa
             .pa1
             .into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr);
-        let b_pa3 = gpioa
+        a_pa1.make_interrupt_source(&mut syscfg);
+        a_pa1.trigger_on_edge(&mut exti, Edge::Rising);
+        a_pa1.enable_interrupt(&mut exti);
+
+        let b_pa3: CartEncIn2 = gpioa
             .pa3
             .into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr);
         let origin_offset_counts = 0;
         let counts_per_rev = 600;
 
-        es38::Encoder::new(a_pa1, b_pa3, counts_per_rev, origin_offset_counts)
+        let cart_encoder: CartEncoder =
+            es38::Encoder::new(a_pa1, b_pa3, counts_per_rev, origin_offset_counts);
 
+        Hardware {
+            motor_driver,
+            pendulum_encoder,
+            cart_encoder,
+        }
     }
 }
-
--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -2,35 +2,35 @@
 #![no_main]
 mod init;
 
-use core::{borrow::Borrow, cell::RefCell, future::pending};
-use core::u16;
+use core::cell::RefCell;
 
 // Allows for communication back to host during panics and dubugging
+// Uncomment when using GDB
+#[allow(unused_imports)]
 use cortex_m_semihosting::{dbg, hprintln};
 use panic_semihosting as _;
 
-use stm32f3xx_hal as hal;
+// Allows real time logging back to the host. Unfortunately, this is not compatible  with the device itself
 use hal::{
+    gpio::ExtiPin,
     interrupt,
-    pac::Interrupt,
+    pac::{Interrupt, NVIC},
 };
+use stm32f3xx_hal as hal;
 
 use init::{CartEncoder, PendulumEncoder};
 
-
-//use cortex_m::asm;
+use cortex_m::asm;
 use cortex_m::interrupt::free as interrupt_free;
 use cortex_m::interrupt::Mutex;
 use cortex_m_rt::entry;
+use defmt::global_logger;
 
-static CART_ENCODER: Mutex<RefCell<Option<CartEncoder>>> =
-    Mutex::new(RefCell::new(None));
-static PENDULUM_ENCODER: Mutex<RefCell<Option<PendulumEncoder>>> =
-    Mutex::new(RefCell::new(None));
+static CART_ENCODER: Mutex<RefCell<Option<CartEncoder>>> = Mutex::new(RefCell::new(None));
+static PENDULUM_ENCODER: Mutex<RefCell<Option<PendulumEncoder>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
-    
     let init::Hardware {
         pendulum_encoder,
         cart_encoder,
@@ -46,23 +46,46 @@ fn main() -> ! {
     // Configuring a LED
     // let mut red_led = gpioe
     //     .pe9
-    //     .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    //  eh   .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
 
+    // Enabling interrupts
+    unsafe {
+        NVIC::unmask(Interrupt::EXTI1);
+        //NVIC::unmask(Interrupt::EXTI15_10);
+    }
     // Blinking the LED for debug purposes
     loop {
-        // red_led.toggle().unwrap();
-        // asm::delay(8_000_000);
-        // red_led.toggle().unwrap();
-        // asm::delay(8_000_000);
+        asm::wfi();
     }
 }
 
-#[interrupt]
-fn EXTI0() {
-    hprintln!("EXTI0 triggered").unwrap();
-}
-
+// Cart Encoder Interrupt
 #[interrupt]
 fn EXTI1() {
-    hprintln!("EXTI1 triggered").unwrap();
+    //hprintln!("Cart Interrupt triggered").unwrap();
+    interrupt_free(|cs| {
+        let mut cart_encoder = CART_ENCODER.borrow(cs).borrow_mut();
+        let cart_encoder = cart_encoder
+            .as_mut()
+            .expect("EXTI1 interrupt was called and CartEncoder was not intialized");
+
+        // Update the angle and direction state of the encoder
+        cart_encoder
+            .update()
+            .expect("Failed to update the position of the encoder");
+
+        defmt::info!("angle: {:f32}", cart_encoder.angle().radians());
+        //hprintln!("angle: {}", cart_encoder.angle().radians()).unwrap();
+
+        cart_encoder
+            .hardware()
+            .pin_a()
+            .clear_interrupt_pending_bit()
+    });
+}
+
+// Pendulum Encoder Interrupt
+#[interrupt]
+fn EXTI15_10() {
+    defmt::info!("")
 }
