69a944d3a62f2277701a5b316f70e6dac8d498a5
seanybaggins <seli2854@colorado.edu>
2021-03-03 08:28:16 -0700

    about to change refences are aquired within interrupts

--- implementation/src/main.rs
+++ implementation/src/main.rs
@@ -25,12 +25,49 @@ use stm32f3xx_hal as hal;
 use init::{CartEncoder, PendulumEncoder, StopWatch};
 
 use cortex_m::interrupt::free as interrupt_free;
-use cortex_m::interrupt::Mutex;
+use cortex_m::interrupt::{CriticalSection, Mutex};
 use cortex_m_rt::entry;
 
 static CART_ENCODER: Mutex<RefCell<Option<CartEncoder>>> = Mutex::new(RefCell::new(None));
 static PENDULUM_ENCODER: Mutex<RefCell<Option<PendulumEncoder>>> = Mutex::new(RefCell::new(None));
 static STOPWATCH: Mutex<RefCell<Option<StopWatch>>> = Mutex::new(RefCell::new(None));
+
+macro_rules! get_global_ref {
+    ($global_item:ident, $cs:ident) => {
+        $global_item
+            .borrow($cs)
+            .borrow()
+            .as_ref()
+            .expect("failed to get global reference")
+    };
+}
+
+macro_rules! get_global_ref_mut {
+    ($global_item:ident, $cs:ident) => {
+        $global_item
+            .borrow($cs)
+            .borrow_mut()
+            .as_mut()
+            .expect("failed to get global mutible reference")
+    };
+}
+
+//fn get_global_ref<'cs, T>(
+//    global_item: &'static Mutex<RefCell<Option<T>>>,
+//    cs: &'cs CriticalSection,
+//) -> &'cs T {
+//    global_item.borrow(cs).borrow().as_ref().unwrap()
+//}
+
+fn get_milli_sec_since_epoch(cs: &CriticalSection) -> Milliseconds<u32> {
+    defmt::trace!("get_milli_sec_since_epoch");
+    get_global_ref!(STOPWATCH, cs)
+        .try_now()
+        .unwrap()
+        .duration_since_epoch()
+        .try_into()
+        .unwrap()
+}
 
 #[entry]
 fn main() -> ! {
@@ -58,39 +95,18 @@ fn main() -> ! {
 
     defmt::trace!("looping forever");
     loop {
-        let cart_angle = interrupt_free(|cs| {
-            CART_ENCODER
-                .borrow(cs)
-                .borrow()
-                .as_ref()
-                .unwrap()
-                .angle()
-                .clone()
-        });
-        let time_since_epoch_milli_sec: Milliseconds<u32> = interrupt_free(|cs| {
-            STOPWATCH
-                .borrow(cs)
-                .borrow()
-                .as_ref()
-                .unwrap()
-                .try_now()
-                .unwrap()
-                .duration_since_epoch()
-                .try_into()
-                .unwrap()
-        });
-        let cart_degrees_per_sec = interrupt_free(|cs| {
-            CART_ENCODER
-                .borrow(cs)
-                .borrow_mut()
-                .as_mut()
-                .unwrap()
-                .velocity()
-                .degrees_per_sec(cart_angle, time_since_epoch_milli_sec)
+        let time_since_epoch_milli_sec = interrupt_free(|cs| get_milli_sec_since_epoch(cs));
+
+        let (cart_angle, cart_velocity) = interrupt_free(|cs| {
+            let cart_angle = get_global_ref!(CART_ENCODER, cs).angle().clone();
+            let cart_velocity = get_global_ref_mut!(CART_ENCODER, cs)
+                .velocity(cart_angle, time_since_epoch_milli_sec);
+
+            (cart_angle, cart_velocity)
         });
 
-        defmt::debug!("cart_angle = {} deg", cart_angle.degrees());
-        defmt::debug!("cart_degree_per_sec = {} deg/sec", cart_degrees_per_sec,);
+        defmt::debug!("cart_velocity: {}", cart_velocity);
+        // defmt::debug!("cart_angle = {} deg", cart_angle.degrees());
 
         cortex_m::asm::delay(16_000_000)
     }
@@ -99,6 +115,7 @@ fn main() -> ! {
 fn update_cart(cs: &cortex_m::interrupt::CriticalSection) {
     defmt::trace!("update cart");
 
+    let millisec_since_epoch = get_milli_sec_since_epoch(cs);
     let mut cart_encoder = CART_ENCODER.borrow(cs).borrow_mut();
     let cart_encoder = cart_encoder
         .as_mut()
@@ -106,7 +123,7 @@ fn update_cart(cs: &cortex_m::interrupt::CriticalSection) {
 
     // Update the angle and direction state of the encoder
     cart_encoder
-        .update()
+        .update(millisec_since_epoch)
         .expect("Failed to update the position of the encoder");
 
     cart_encoder
